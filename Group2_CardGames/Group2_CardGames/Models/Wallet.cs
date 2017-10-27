fb4b957078577a300220f46a15ca63a66a335502
BorisHo <BHo@student.neumont.edu>
2017-10-27 08:56:02 -0600

    Made Wallet Class

---
+++ Group2_CardGames/Group2_CardGames/Models/Wallet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2_CardGames.Models
+{
+    public class Wallet
+    {
+        public int Money { get; set; }
+
+        public Wallet(int money)
+        {
+            this.Money = money;
+        }
+
+        public int sendMoney(int money)
+        {   
+            return Money -= money;
+        }
+        public int receiveMoney(int money)
+        {
+            return Money += money;
+        }
+    }
+}
